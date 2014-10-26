3fcf32a0dfce1ae1fcf2966aa6354d9cfe122fff
Michael Yagudaev <michael@yagudaev.com>
2014-10-25 21:23:34 -0700

    Initial Commit

---
+++ HelloMyoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloMyoSwift
+//
+//  Created by Michael Yagudaev on 2014-10-25.
+//  Copyright (c) 2014 Michael Yagudaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
